edc1dab488f07840f5a18caded1d6a5f03ba4b85
DJHeadset <zoltan.blaho.83@gmail.com>
2026-02-28 11:33:53 +0100

    router routes fix

--- routes/choresRoute.js
+++ routes/choresRoute.js
@@ -8,10 +8,10 @@ const {
   scoreUpdate,
 } = require("../controllers/choresController");
 
-router.route("/complete_chore", completeChore);
-router.route("/update_chore", regenerateChores);
-router.route("/update_hourly", resetHourly);
-router.route("/update_midnight", resetMidnight);
-router.route("/update_score", scoreUpdate);
+router.post("/complete_chore", completeChore);
+router.post("/update_chore", regenerateChores);
+router.post("/update_hourly", resetHourly);
+router.post("/update_midnight", resetMidnight);
+router.post("/update_score", scoreUpdate);
 
 module.exports = router;
